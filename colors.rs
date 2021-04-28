e62b1e1169642e8b559d8ee6970ff96e7fc7f96d
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-04-28 18:00:17 -0400

    improve `heat` function + small fixes

--- colors.rs
+++ colors.rs
@@ -3,34 +3,36 @@ use std::cmp;
 #[allow(dead_code)]
 pub fn amber(density: u8) -> [u8; 3] {
 	[
-		cmp::max(255 - (density as i16) * 4, 0) as u8,
-		cmp::max(255 - (density as i16) * 8, 0) as u8,
-		cmp::max(255 - (density as i16) * 20, 0) as u8
+		255 - cmp::min((density as u16) * 4, 255) as u8,
+		255 - cmp::min((density as u16) * 8, 255) as u8,
+		255 - cmp::min((density as u16) * 20, 255) as u8
 	]
 }
 
 #[allow(dead_code)]
 pub fn sparks(density: u8) -> [u8; 3] {
 	[
-		cmp::min((density as i16) * 18, 255) as u8,
-		cmp::min((density as i16) * 10, 255) as u8,
-		cmp::min((density as i16) * 4, 255) as u8
+		cmp::min((density as u16) * 18, 255) as u8,
+		cmp::min((density as u16) * 10, 255) as u8,
+		cmp::min((density as u16) * 4, 255) as u8
 	]
 }
 
 #[allow(dead_code)]
 pub fn heat(density: u8) -> [u8; 3] {
-	let d1 = cmp::min((density as u16) * 4, 255) as u8;
-	let d2 = cmp::min((density as u16) * 10, 255) as u8;
-	let d3 = cmp::min((density as u16) * 18, 255) as u8;
+	let d4 = cmp::min((density as u16) * 4, 255) as u8;
+	let d12 = cmp::min((density as u16) * 12, 255) as u8;
+	let d22 = cmp::min((density as u16) * 22, 255) as u8;
 
 	if density < 7 {
-		[d1, 0, d3]
+		[d4, 0, d22]
 	} else if density < 12 {
-		[0, d2, d3]
-	} else if density < 24 {
-		[d1, d2, 0]
+		[0, d12, 255 - d22]
+	} else if density < 22 {
+		[d12, d12, 0]
+	} else if density < 190 {
+		[d22, 255 - d4, 0]
 	} else {
-		[d3, d1, 0]
+		[density, density, density]
 	}
 }
