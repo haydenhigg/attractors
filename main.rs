aa9b4931fde482b1f393d3523386c01373ae67ec
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-04-28 09:16:40 -0400

    make default params more interesting

--- main.rs
+++ main.rs
@@ -2,9 +2,9 @@ mod attractor;
 mod colors;
 mod file;
 
-const ITERS: usize	= 100000000;
-const WIDTH: usize	= 6000;
-const HEIGHT: usize	= 6000;
+const ITERS: usize	= 90000000;
+const WIDTH: usize	= 4000;
+const HEIGHT: usize	= 4000;
 const OUTFILE: &str	= "out.ppm";
 
 fn main() {
@@ -12,9 +12,9 @@ fn main() {
 	// -2.0, -2.0, -1.2, 2.0
 	// 0.56, -5.6, -1.9, 2.0
 
-	let points = attractor::generate(ITERS, -2.0, -2.0, -1.2, 2.0);
+	let points = attractor::generate(ITERS, 2.01, 2.53, 1.61, -0.33);
 	let histogram = attractor::make_histogram(WIDTH, HEIGHT, points);
-	let image_data = file::make_image_data(WIDTH, HEIGHT, histogram, colors::amber);
+	let image_data = file::make_image_data(WIDTH, HEIGHT, histogram, colors::heat);
 
 	if let Ok(()) = file::write_to_ppm(WIDTH, HEIGHT, image_data, OUTFILE) {
 		println!("success");
