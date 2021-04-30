98a9e7bb36fe4583a97a6e04a25221b3d83b6201
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-04-30 08:34:19 -0400

    add `test_color` function

--- file.rs
+++ file.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{Result, Write};
 
-//-- make a 1D Vec of bytes from the 2D histogram
+//-- make a 1D Vec of bytes from the 2D histogram --//
 pub fn make_image_data(w: usize, h: usize, histogram: Vec<Vec<u8>>, color: fn(u8) -> [u8; 3]) -> Vec<u8> {
 	//-- find lowest and highest densities --//
 	let mut min = u8::MAX;
@@ -43,6 +43,25 @@ pub fn write_to_ppm(w: usize, h: usize, image_data: Vec<u8>, file_name: &'static
 
 	writeln!(file, "P6 {} {} {}", w, h, u8::MAX)?; // header | binary, 8-bit PPM file
 	file.write_all(&image_data)?;                  // data   | 1 call is seemingly the most efficient way
+
+	Ok(())
+}
+
+//-- create a test image for color function --//
+#[allow(dead_code)]
+pub fn test_color(color: fn(u8) -> [u8; 3], file_name: &'static str) -> Result<()> {
+	let mut image_data: Vec<u8> = Vec::with_capacity(768);
+
+	for i in 0..=255 {
+		for j in 0..=255 {
+			image_data.extend_from_slice(&color((i as f64 * j as f64 / 255.0) as u8));
+		}
+	}
+
+	let mut file = File::create(file_name)?;
+
+	writeln!(file, "P6 256 256 255")?; // header | binary, 8-bit PPM file
+	file.write_all(&image_data)?;      // data   | 1 call is seemingly the most efficient way
 
 	Ok(())
 }
