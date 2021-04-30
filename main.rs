85ad22fd9b1724aaf2545c586431075cc4630576
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-04-30 08:54:25 -0400

    make defaults more interesting

--- main.rs
+++ main.rs
@@ -2,19 +2,21 @@ mod attractor;
 mod colors;
 mod file;
 
-const ITERS: usize	= 90000000;
+const ITERS: usize	= 140000000;
 const WIDTH: usize	= 4000;
 const HEIGHT: usize	= 4000;
 const OUTFILE: &str	= "out.ppm";
 
 fn main() {
-	// 2.01, 2.53, 1.61, -0.33
-	// -2.0, -2.0, -1.2, 2.0
-	// 0.56, -5.6, -1.9, 2.0
+	// 2.01, 2.53, 1.61, -0.33             fish
+	// -2.0, -2.0, -1.2, 2.0               cornucopia
+	// 0.56, -5.6, -1.9, 2.0               whirlpool
+	// 1.78125, -0.78125, 1.90625, 2.65625 jellyfish
+	// -2.487, -1.888, 1.233, 3.862	       shell
 
-	let points = attractor::generate(ITERS, 2.01, 2.53, 1.61, -0.33);
+	let points = attractor::generate(ITERS, -2.487, -1.888, 1.233, 3.862);
 	let histogram = attractor::make_histogram(WIDTH, HEIGHT, points);
-	let image_data = file::make_image_data(WIDTH, HEIGHT, histogram, colors::heat);
+	let image_data = file::make_image_data(WIDTH, HEIGHT, histogram, colors::viridis);
 
 	if let Ok(()) = file::write_to_ppm(WIDTH, HEIGHT, image_data, OUTFILE) {
 		println!("success");
